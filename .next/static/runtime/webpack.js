61128962ea3293ef4311bfd29b3a30ac6635602a
Drew Blincoe <drewblincoe@yahoo.com>
2021-06-11 13:00:23 -0500

    updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a933ab24719b846c0b2";
+/******/ 	var hotCurrentHash = "c680f6359110f9031e1f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
